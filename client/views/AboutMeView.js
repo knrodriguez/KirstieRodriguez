ba761dd02c8a4f57de802330860a4697ca33cae8
K <KirstieNRodriguez@gmail.com>
2021-06-19 16:28:49 -0400

    refactor: adds thought bubble to about me view

--- client/views/AboutMeView.js
+++ client/views/AboutMeView.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { ThoughtBubble } from '../components';
+import { consts } from '../reducers/constants'
 
 export default (props) => {
+    const { sizes, types } = consts;
     return (
         <>
             Inside Kirstie's Mind
+            <ThoughtBubble 
+                size={sizes.MICRO} 
+                type={types.ROUND} 
+                outline={true} 
+            />
+            {/* <ThoughtBubble 
+                size={sizes.MD} 
+                type={types.ROUND} 
+                outline={true} 
+            /> */}
         </>
     )
 }
--- client/views/index.js
+++ client/views/index.js
@@ -1,4 +1,4 @@
-export { default as AboutMeView } from './AboutMeViewView'
+export { default as AboutMeView } from './AboutMeView'
 export { default as ExperienceView } from './ExperienceView'
 export { default as ProjectsView } from './ProjectsView'
 export { default as TechStackView } from './TechStackView'
