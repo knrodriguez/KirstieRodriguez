924d48a150e41d8357aea9fb4a1cbc646caf22fa
K <KirstieNRodriguez@gmail.com>
2021-06-19 16:29:39 -0400

    feat: connects to port and opens server, updates static file paths

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('*', (req,res,next) => {
     try {
-        res.sendFile(path.join(__dirname, 'public/index.html'))
+        res.sendFile(path.join(__dirname, '../public/index.html'))
     } catch (error) {
         next(error);
     }
@@ -13,4 +15,9 @@ app.get('*', (req,res,next) => {
 
 app.use((err,req,res,next) => {
     res.status(err.status || 500).send(err.message || 'Internal Server Error');
+})
+
+app.listen(PORT, () => {
+    console.log(`Connected on port ${PORT} at \n
+        http://localhost:${PORT}`);
 })
