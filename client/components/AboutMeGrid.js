a40edc6d67093e13129e999100b8f99e7e19659a
K <KirstieNRodriguez@gmail.com>
2021-07-11 23:03:42 -0400

    feat: creates branch for grid

--- client/components/AboutMeGrid.js
+++ client/components/AboutMeGrid.js
@@ -3,15 +3,15 @@ import React from 'react';
 export default (props) => {
     return(
         <div class='page-container'>
-            <div class='blue'></div>
-            <div class='red'></div>
-            <div class='green'></div>
-            <div class='black'></div>
-            <div class='blue'></div>
-            <div class='red'></div>
-            <div class='green'></div>
-            <div class='black'></div>
-            <div class='yellow'></div>
+            <div class='blue'>a</div>
+            <div class='red'>b</div>
+            <div class='green'>c</div>
+            <div class='black'>d</div>
+            <div class='blue'>e</div>
+            <div class='red'>f</div>
+            <div class='green'>g</div>
+            <div class='black'>h</div>
+            <div class='yellow'>i</div>
         </div>
     )
 }
