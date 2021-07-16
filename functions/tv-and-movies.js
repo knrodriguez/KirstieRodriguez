d9729a0cd34640ca15fa9f50d7151cb7cad245ae
K <KirstieNRodriguez@gmail.com>
2021-07-15 23:08:42 -0400

    refactor: css ofabout me container to fit screen

--- functions/tv-and-movies.js
+++ functions/tv-and-movies.js
@@ -4,8 +4,6 @@ const { Media } = require("../server/models");
 exports.handler = async (event, context) => {
   const configUrl = `https://api.themoviedb.org/3/configuration?api_key=${process.env.TMDB_API_KEY_V3}`;
   const listUrl = `https://api.themoviedb.org/4/list/${process.env.LIST_ID}?api_key=${process.env.TMDB_API_KEY_V3}`;
-
-  console.log('INSIDE THE TV AND MOVIES HANDLER', process.env.LIST_ID);
   
   try {
     const { data: config } = await axios.get(configUrl);
@@ -14,8 +12,6 @@ exports.handler = async (event, context) => {
         "Content-Type": "application/json;charset=utf-8",
       },
     });
-
-    console.log(config,list)
 
     const media = list.results.map((record) => {
       return new Media(config, record);
